e9c8b80698a1ef64bcb7a6b8a251232086d863f6
Figurinha20 <figurinhapt@gmail.com>
2020-06-18 22:09:34 +0100

    Ecrã Adicionado! Tentativa de adicionar cadeira...

--- Car_sol.js
+++ Car_sol.js
@@ -2,7 +2,7 @@ let renderer = null,
     scene = null,
     camera1 = null,
     camera2 = null,
-    car;   // The three.js object that represents the model
+    car; // The three.js object that represents the model
 
 let track, stadium, plane, checkpointObj;
 
@@ -10,15 +10,15 @@ let track, stadium, plane, checkpointObj;
 let texTrack = new THREE.TextureLoader().load("./images/texTrack.png");
 let texMarbleStadium = new THREE.TextureLoader().load("./images/texture.JPG");
 
-
-
-
 let laps = 0;
 let activeCheckpoint = 0;
 let collision = false;
+let text, screenText;
+
 
 let raycaster = new THREE.Raycaster();
-let rayDirection = new THREE.Vector3( 0, -1, 0 );
+let fontLoader = new THREE.FontLoader();
+let rayDirection = new THREE.Vector3(0, -1, 0);
 let intersects;
 
 let camera1isActive = true;
@@ -26,10 +26,6 @@ let camera1isActive = true;
 //colisões?
 let BBox;
 let BBox2;
-
-
-
-
 
 window.onload = function init() {
     // Create the Three.js renderer
@@ -44,30 +40,30 @@ window.onload = function init() {
 
     // Adicionar background
     //background
-   
-   
-    let textureCube = new THREE.CubeTextureLoader().setPath( './cube/' ).load(
-            [ 'left.JPG', 'right.JPG', 'top.JPG', 'bottom.JPG', 'front.JPG', 'back.JPG' ] ,
+
+
+    let textureCube = new THREE.CubeTextureLoader().setPath('./cube/').load(
+        ['left.JPG', 'right.JPG', 'top.JPG', 'bottom.JPG', 'front.JPG', 'back.JPG'],
         // onLoad callback
         // Here the loaded data is assumed to be an object
-        function ( object ) {
-           console.log("sucess");
+        function (object) {
+            console.log("sucess");
 
-           
+
         },
-    
+
         // onProgress callback
-        function ( xhr ) {
-            console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
         },
-    
+
         // onError callback
-        function ( err ) {
-            console.error( 'An error happened' );
+        function (err) {
+            console.error('An error happened');
         }
     );
-        console.log(scene.background);
-        
+    console.log(scene.background);
+
     scene.background = textureCube;
 
     // Add TWO cameras 
@@ -85,27 +81,24 @@ window.onload = function init() {
 
     // Lights
 
-        //directional light
-    let moonlight = new THREE.DirectionalLight( "#e1fafc", 0.3 );
+    //directional light
+    let moonlight = new THREE.DirectionalLight("#e1fafc", 0.3);
     moonlight.castShadow = true;
-    scene.add( moonlight );
+    scene.add(moonlight);
 
 
+    let spotLightRight = new THREE.SpotLight(0xffffff, 0.8, 800, Math.PI / 2, 0.3, 0.5);
+    spotLightRight.position.set(500, 200, 200);
 
-  
-    
-    let spotLightRight = new THREE.SpotLight( 0xffffff, 0.8 , 800 , Math.PI/2 , 0.3, 0.5 );
-    spotLightRight.position.set( 500, 200, 200 );
-    
     spotLightRight.castShadow = false;
 
-    let spotLightLeft = new THREE.SpotLight( 0xffffff, 0.8 , 800 , -Math.PI/2 , 0.3, 0.5 );
+    let spotLightLeft = new THREE.SpotLight(0xffffff, 0.8, 800, -Math.PI / 2, 0.3, 0.5);
     spotLightLeft.position.set(-500, 200, 200);
-    
+
     spotLightLeft.castShadow = false;
 
-    let spotLight = new THREE.SpotLight( 0xffffff, 1 , 1500 , Math.PI/16 , 0.9, 0.3 );
-    spotLight.position.set( 0, 500, 200 );
+    let spotLight = new THREE.SpotLight(0xffffff, 1, 1500, Math.PI / 16, 0.9, 0.3);
+    spotLight.position.set(0, 500, 200);
     spotLight.castShadow = true;
 
     spotLight.shadow.mapSize.width = 400;
@@ -113,67 +106,66 @@ window.onload = function init() {
 
     spotLight.castShadow = false;
 
-    scene.add( spotLightRight );
-    scene.add( spotLightLeft );
-    scene.add( spotLight );
-
-    
-
+    scene.add(spotLightRight);
+    scene.add(spotLightLeft);
+    scene.add(spotLight);
 
     //Shadows
     renderer.shadowMap.enabled = true;
 
-    
     //let material = new THREE.MeshBasicMaterial( {color: "#33cc0c", side: THREE.DoubleSide} );
     let texture = new THREE.TextureLoader().load("./images/grass.jpg");
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set( 200, 200 );
+    texture.repeat.set(200, 200);
 
-    let material = new THREE.MeshPhongMaterial( { map: texture, side: THREE.DoubleSide} )
-    
+    let material = new THREE.MeshPhongMaterial({
+        map: texture,
+        side: THREE.DoubleSide
+    })
+
     //floor
-    let geometry = new THREE.PlaneGeometry( 9000, 9000, 0 );
-    plane = new THREE.Mesh( geometry, material );
+    let geometry = new THREE.PlaneGeometry(9000, 9000, 0);
+    plane = new THREE.Mesh(geometry, material);
 
     plane.receiveShadow = true;
 
-    plane.rotation.set(3*Math.PI/2,0,0)
-    plane.position.set(0,-1,0)
-    scene.add( plane );
-    
+    plane.rotation.set(3 * Math.PI / 2, 0, 0);
+    plane.position.set(0, -1, 0);
+    scene.add(plane);
+
 
 
 
     //add Track
     let loader = new THREE.ObjectLoader();
 
-    
+
 
     loader.load(
         // resource URL
         "models/track.json",
-    
+
         // onLoad callback
         // Here the loaded data is assumed to be an object
-        function ( object ) {
+        function (object) {
             track = object;
             track.receiveShadow = true;
 
             // Add the loaded object to the scene
-            scene.add( track );
-            track.scale.set(140,140,140)
+            scene.add(track);
+            track.scale.set(140, 140, 140)
 
         },
-    
+
         // onProgress callback
-        function ( xhr ) {
-            console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
         },
-    
+
         // onError callback
-        function ( err ) {
-            console.error( 'An error happened' );
+        function (err) {
+            console.error('An error happened');
         }
     );
 
@@ -182,54 +174,54 @@ window.onload = function init() {
     loader.load(
         // resource URL
         "models/tv.json",
-    
+
         // onLoad callback
         // Here the loaded data is assumed to be an object
-        function ( object ) {
+        function (object) {
             tv = object;
 
             tv.scale.set(140, 140, 140);
             tv.position.set(0, 140, -740);
 
             // Add the loaded object to the scene
-            scene.add( tv );
+            scene.add(tv);
 
         },
-    
+
         // onProgress callback
-        function ( xhr ) {
-            console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
         },
-    
+
         // onError callback
-        function ( err ) {
-            console.error( 'An error happened' );
+        function (err) {
+            console.error('An error happened');
         }
     );
 
     loader.load(
         // resource URL
         "models/stadium.json",
-    
+
         // onLoad callback
         // Here the loaded data is assumed to be an object
-        function ( object ) {
+        function (object) {
             stadium = object;
             // Add the loaded object to the scene
-            
-            stadium.scale.set(180,100,180);
-            stadium.position.set(0,-90,0);
-            scene.add( stadium );
+
+            stadium.scale.set(180, 100, 180);
+            stadium.position.set(0, -90, 0);
+            scene.add(stadium);
         },
-    
+
         // onProgress callback
-        function ( xhr ) {
-            console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
         },
-    
+
         // onError callback
-        function ( err ) {
-            console.error( 'An error happened' );
+        function (err) {
+            console.error('An error happened');
         }
     );
 
@@ -245,18 +237,36 @@ window.onload = function init() {
             car.receiveShadow = true;
             car.castShadow = true;
             //let axes = new THREE.AxesHelper(100);
-            
+
             //car.add(axes)
             scene.add(car);
-            
+
             spotLight.target = car;
         });
+    });
+
+    //Escrever texto inicial
+    fontLoader.load('./fonts/Hemi Head Rg_Bold Italic.json', function (font) {
+
+        text = new THREE.TextGeometry(`Laps ${laps}/3`, {
+            font: font,
+            size: 29,
+            height: 3,
+            curveSegments: 12,
+        });
+        
+        let textMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+        screenText = new THREE.Mesh( text, textMaterial );
+        scene.add( screenText );
+        screenText.position.x=-90
+        screenText.position.y=126;
+        screenText.position.z=-690;
     });
 
     // let controls = new THREE.OrbitControls(camera);
     // controls.addEventListener('change', function () { renderer.render(scene, camera); });
 
-    
+
     // Run the run loop
     render();
 }
@@ -271,22 +281,19 @@ let speed = 0;
 
 function render() {
     if (car != undefined) {
-        
+
         // sets the toycar object with the updated position
         //car.position.set(pos.x, pos.y, pos.z);
-        
+
         //Colisoes usando RayCaster
         let origin = car.position.clone();
         origin.y += 5;
-        raycaster.set(origin, rayDirection)
+        raycaster.set(origin, rayDirection);
 
         //reset de intersects
         intersects = [];
         // calculate objects intersecting the picking ray
-        intersects = raycaster.intersectObjects( track.children, true );
-
-
-        
+        intersects = raycaster.intersectObjects(track.children, true);
 
         // rotates the car by angle radians
         car.rotation.y = angle;
@@ -302,10 +309,10 @@ function render() {
         car.position.z += speed * Math.cos(angle);
 
         //colisão com coliseu
-        if(car.position.z >= 610 || car.position.z <= -610 || car.position.x >= 720 || car.position.x <= -720){
-            car.position.x -= speed*3 * Math.sin(angle);
-            car.position.z -= speed *3 * Math.cos(angle);
-            speed*=-0.2;
+        if (car.position.z >= 610 || car.position.z <= -610 || car.position.x >= 720 || car.position.x <= -720) {
+            car.position.x -= speed * 3 * Math.sin(angle);
+            car.position.z -= speed * 3 * Math.cos(angle);
+            speed *= -0.2;
         }
 
         //Checkpoint e Laps | 3 Laps = Win | 4 Checkpoints = 1 Lap
@@ -324,9 +331,9 @@ function render() {
     }
 
 
-    if (camera1isActive){
+    if (camera1isActive) {
         renderer.render(scene, camera1);
-    }else{
+    } else {
         renderer.render(scene, camera2);
     }
     requestAnimationFrame(render);
@@ -358,17 +365,17 @@ document.addEventListener('keyup', (e) => {
             break;
         default:
             break;
-    }  
+    }
 });
 
 document.addEventListener('keydown', (e) => {
     e.preventDefault();
-    
+
     switch (e.key) {
         case 'ArrowUp':
             keysPressed.arrowUp = true;
             break;
-    
+
         case 'ArrowDown':
             keysPressed.arrowDown = true;
             break;
@@ -380,19 +387,19 @@ document.addEventListener('keydown', (e) => {
             break;
         default:
             break;
-    }    
+    }
 
-    if (e.keyCode == 49){    //1
+    if (e.keyCode == 49) { //1
         camera1isActive = true;
     }
-    if (e.keyCode == 50){    //2
+    if (e.keyCode == 50) { //2
         camera1isActive = false;
     }
 });
 
 
-function manageMovement(){
-    
+function manageMovement() {
+
     if (keysPressed.arrowUp == true) {
         forwardSpeed();
     }
@@ -408,94 +415,134 @@ function manageMovement(){
         turnRight();
     }
 
-    function forwardSpeed(){
+    function forwardSpeed() {
         speed += 0.15;
-        if (speed > 6){
+        if (speed > 6) {
             speed = 6;
         }
     }
-    function backwardSpeed(){
+
+    function backwardSpeed() {
         speed -= 0.15;
-        if (speed < -1){
+        if (speed < -1) {
             speed = -1;
         }
     }
 
-    function turnLeft(){
+    function turnLeft() {
         angle += 0.06;
     }
-    function turnRight(){
-        angle -= 0.06; 
+
+    function turnRight() {
+        angle -= 0.06;
     }
 }
 
-function atrition(){
-  //interseção com relva (diminui speed drasticamente)
-        if (intersects.length == 0){
-            speed=speed*0.85;
-        }
-
-        if(speed > 0){
-            speed -= speed*0.03;
-        }
-        if(speed < 0){
-            speed += speed*0.03;
-        }
-        if(speed < 0.07 && speed > -0.07){
-            speed = 0;
-        }
+function atrition() {
+    //interseção com relva (diminui speed drasticamente)
+    if (intersects.length == 0) {
+        speed = speed * 0.85;
+    }
+    if (speed > 0) {
+        speed -= speed * 0.03;
+    }
+    if (speed < 0) {
+        speed += speed * 0.03;
+    }
+    if (speed < 0.07 && speed > -0.07) {
+        speed = 0;
+    }
 }
 
-function createCheckpoint(checkpoint){
-    
-    scene.remove (checkpointObj);
+function createCheckpoint(checkpoint) {
+
+    scene.remove(checkpointObj);
 
     let geometry = new THREE.CylinderGeometry(65, 65, 25, 32);
-    let material = new THREE.MeshBasicMaterial( {color: 0xffff00, opacity: 0.4, transparent: true} );
-    checkpointObj = new THREE.Mesh( geometry, material );
+    let material = new THREE.MeshBasicMaterial({
+        color: 0xffff00,
+        opacity: 0.4,
+        transparent: true
+    });
+    checkpointObj = new THREE.Mesh(geometry, material);
     //console.log("Checkpoints:" + checkpoint + " | Laps:" + laps + "/3")
 
-    switch(checkpoint) {
+    switch (checkpoint) {
         case 0:
-          checkpointObj.position.z = 118;
-          break;
+            checkpointObj.position.z = 118;
+            break;
         case 1:
             checkpointObj.position.x = -450;
             checkpointObj.position.z = -100;
-          break;
+            break;
         case 2:
             checkpointObj.position.z = -380;
-          break;
+            break;
         case 3:
             checkpointObj.position.x = 450;
             checkpointObj.position.z = -85;
-           break;
+            break;
         default:
-          console.log("The Checkpoints Broke")
-      }
+            console.log("The Checkpoints Broke");
+    }
 
     scene.add(checkpointObj);
 }
 
-function checkCheckpointColision(){
+function checkCheckpointColision() {
     BBox = new THREE.Box3().setFromObject(car);
     BBox2 = new THREE.Box3().setFromObject(checkpointObj);
 
     collision = BBox.intersectsBox(BBox2); // checks collision between mesh and othermesh
 
-    if (collision==true){
-        if(activeCheckpoint<3){
-            activeCheckpoint++
-        }
-        else{
-            activeCheckpoint = 0
-            laps++
+    if (collision == true) {
+        if (activeCheckpoint < 3) {
+            activeCheckpoint++;
+        } else {
+            activeCheckpoint = 0;
+            laps++;
+
+            scene.remove(screenText);
+
+            //escrever novo texto
+            fontLoader.load('./fonts/Hemi Head Rg_Bold Italic.json', function (font) {
+        
+                text = new THREE.TextGeometry(`Laps ${laps}/3`, {
+                    font: font,
+                    size: 29,
+                    height: 3,
+                    curveSegments: 12,
+                });
+                
+                let textMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+                screenText = new THREE.Mesh( text, textMaterial );
+                scene.add( screenText );
+                screenText.position.x=-80-5*laps;
+                screenText.position.y=126;
+                screenText.position.z=-690;
+            });
         }
     }
 
-    if (laps==3 && activeCheckpoint==1){
-        console.log("You Won!")
+    if (laps == 3 && activeCheckpoint == 1) {
+
+        scene.remove(screenText);
+        //Jogo ganho! Escrever texto final
+        fontLoader.load('./fonts/Hemi Head Rg_Bold Italic.json', function (font) {
+    
+            text = new THREE.TextGeometry(`You Won`, {
+                font: font,
+                size: 29,
+                height: 3,
+                curveSegments: 12,
+            });
+            
+            let textMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+            screenText = new THREE.Mesh( text, textMaterial );
+            scene.add( screenText );
+            screenText.position.x=-92;
+            screenText.position.y=126;
+            screenText.position.z=-690;
+        });
     }
 }
-
-
